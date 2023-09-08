1656c11e4d6f79c6280ea370242a3240e95bd0a2
black_hole <sdarksoul99@gmail.com>
2023-09-08 10:55:14 +0200

    WPManager-commit
    Major update: Implementation of WPManager.cs for the logic of links and waypoints

---
+++ Assets/Scripts/WPManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct Link
+{
+    public enum direction {UNI, BI};
+    
+    public GameObject node1;
+    public GameObject node2;
+    public direction dir;
+}
+
+
+public class WPManager : MonoBehaviour
+{
+   public GameObject[] waypoints;
+   public Link[] links;
+}
